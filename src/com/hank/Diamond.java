2c020e4bda603df188747392d5f0c8a4414cb630
hanko <hankong.sg@gmail.com>
2022-01-28 22:18:32 +0800

    res

---
+++ src/com/hank/Diamond.java
@@ -0,0 +1,9 @@
+package com.hank;
+
+public class Diamond {
+
+    public static void diammond(String[] args){
+        System.out.println("1");
+    }
+
+}
---
+++ src/com/hank/Main.java
@@ -0,0 +1,49 @@
+package com.hank;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i;
+        int j;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("please enter a no: ");
+        int numberEntered = scanner.nextInt();
+
+    // Print the top part of diamond
+        for (i=1; i<=numberEntered; i++) {
+            //print the spaces
+            for (j=1; j <= numberEntered-i; j++){
+                System.out.print(" ");
+            }
+            //print the *
+            for (j=1; j<= i*2-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // Print the bottom part of diamond
+        for (i=numberEntered-1; i>0; i--) {
+            //print the spaces
+            for (j=1; j <= numberEntered-i; j++){
+                System.out.print(" ");
+            }
+            //print the *
+            for (j=1; j<= i*2-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+
+
+
+    }
+}
+
